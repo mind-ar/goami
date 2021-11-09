718c4b2658fd926d50747c6c6728a79ff309adee
Manuel N <yo.elmanu@gmail.com>
2021-11-09 01:44:28 +0000

    spool: soporte para callback

--- spool/spool.go
+++ spool/spool.go
@@ -17,11 +17,11 @@ func NewSpool(callLimit int) (*Spool, error) {
 	return &Spool{
 		//ctx:      ctx,
 		mutex: new(sync.Mutex),
-		queue: make(chan ami.OriginateData, callLimit-1),
+		queue: make(chan spoolCall, callLimit-1),
 		// ready: make(chan bool, 1),
 		// maxChannels: callLimit,
 		activeChannels: 0,
-		GuardTime: 1000,
+		GuardTime:      1000,
 	}, nil
 }
 
@@ -29,21 +29,29 @@ func NewSpool(callLimit int) (*Spool, error) {
 type Spool struct {
 	ctx   context.Context
 	mutex *sync.Mutex
-	queue chan ami.OriginateData
+	queue chan spoolCall
 	// ready chan bool
 	// maxChannels int
 	activeChannels int
-	OnOriginate func(ami.Response)
-	OnHangup func(ami.Response)
-	GuardTime time.Duration
+	OnOriginate    func(ami.Response)
+	OnHangup       func(ami.Response)
+	GuardTime      time.Duration
+}
+
+type spoolCall struct {
+	data     ami.OriginateData
+	callback func(ami.Response)
 }
 
 //Originate agrega una nueva llamada a la cola de llamadas del discador
-func (s *Spool) Originate(data ami.OriginateData) error {
-	// select {
-	// case   <- s.ready:
-		s.queue <- data
-	// }
+func (s *Spool) Originate(data ami.OriginateData, cb func(ami.Response)) error {
+
+	call := spoolCall{
+		data:     data,
+		callback: cb,
+	}
+
+	s.queue <- call
 	return nil
 }
 
@@ -56,77 +64,72 @@ func (s *Spool) start(ctx context.Context, pool *ami.Pool) error {
 	for {
 		select {
 		case call := <-s.queue:
-			func(){
+			data := call.data
+			cb := call.callback
+			response := func() ami.Response {
 				//originate forzado a asincrónico
-				call.Async = "yes"
+				data.Async = "yes"
 
 				/*
-				//contador de llamadas activas (uso futuro?)
-				s.mutex.Lock()
-				s.activeChannels ++
-				s.mutex.Unlock()
-				defer func() {
+					//contador de llamadas activas (uso futuro?)
 					s.mutex.Lock()
-					s.activeChannels --
+					s.activeChannels ++
 					s.mutex.Unlock()
-				}()
+					defer func() {
+						s.mutex.Lock()
+						s.activeChannels --
+						s.mutex.Unlock()
+					}()
 				*/
-				
+
 				socket, err := pool.GetSocket()
 				if err != nil {
 					log.Error().Err(err).Msg("Error al Obtener llamada del pool")
-					s.OnOriginate(nil)
-					return
+					return nil
 				}
 				defer pool.Close(socket, false)
 
 				uuid, _ := ami.GetUUID()
 
-				call.ChannelID = uuid
-				// log.Trace().Interface("call", call).Msg("analizando llamada en queue")
-				response, err := ami.Originate(ctx, socket, uuid, call)
+				data.ChannelID = uuid
+				// log.Trace().Interface("data", data).Msg("analizando llamada en queue")
+				response, err := ami.Originate(ctx, socket, uuid, data)
 				if err != nil {
 					log.Error().Err(err).Msg("Error al enviar llamada")
-					if s.OnOriginate != nil {
-						s.OnOriginate(nil)
-					}
-					return
+					return response
 				}
-				log.Trace().Interface("response", response).Msg("Originate completado")
+				// log.Trace().Interface("response", response).Msg("Originate completado")
 				resultado := response["Response"][0]
 				if resultado != "Success" {
-					if s.OnOriginate != nil {
-						s.OnOriginate(response)
-					}
- 					return
+					return response
 				}
 				// actionID := response["ActionID"][0]
 				// log.Debug().Interface("response", response).Interface("ActionID", actionID).Msg("")
 
 				/*
-				Posibles resultados en evento OriginateResponse
-				0 = no such extension or number. Also bad dial tech ie. name of a sip trunk that doesn’t exist
-				3 = no anwer
-				4 = answered
-				5 = busy
-				8 = congested or not available (Disconnected Number)
+					Posibles resultados en evento OriginateResponse
+					0 = no such extension or number. Also bad dial tech ie. name of a sip trunk that doesn’t exist
+					3 = no anwer
+					4 = answered
+					5 = busy
+					8 = congested or not available (Disconnected Number)
 				*/
 				response, _ = waitForEvent(ctx, socket, uuid, "OriginateResponse")
-				if s.OnOriginate != nil {
-					s.OnOriginate(response)
-				}
-				// log.Trace().Interface("response", response).Msg("Resultado Originate")
 				resultado = response["Response"][0]
 				if resultado != "Success" {
-					// log.Error().Interface("Message",response["Message"]).Msg("Error al enviar llamada")
-					return
-				}
-				response, _ = waitForEvent(ctx, socket, uuid, "Hangup")
-				if s.OnHangup != nil {
-					s.OnHangup(response)
+					return response
 				}
 
+				response, _ = waitForEvent(ctx, socket, uuid, "Cdr")
+				// if s.OnHangup != nil {
+				// 	s.OnHangup(response)
+				// }
+				return response
+
 			}()
+			if cb != nil {
+				cb(response)
+			}
 			time.Sleep(s.GuardTime * time.Millisecond)
 		}
 
@@ -137,23 +140,32 @@ func (s *Spool) start(ctx context.Context, pool *ami.Pool) error {
 	}
 }
 
-
-
 func waitForEvent(ctx context.Context, socket ami.Client, uuid string, event string) (ami.Response, error) {
 	// log.Debug().Str("uuid", uuid).Str("event", event).Msg("Esperando Evento")
 	for {
 		response, _ := ami.Events(ctx, socket)
-		if len(response["Uniqueid"]) == 0 || response["Uniqueid"][0] != uuid {
+		var responseUUID string
+		// log.Debug().Interface("response", response).Interface("Uniqueid", response["Uniqueid"]).Msg("")
+		if len(response["Uniqueid"]) == 0 {
+			if len(response["UniqueID"]) == 0 {
+				continue
+			}
+			responseUUID = response["UniqueID"][0]
+		} else {
+			responseUUID = response["Uniqueid"][0]
+		}
+
+		if responseUUID != uuid {
 			continue
 		}
+
 		evt := response["Event"][0]
-		// log.Debug().Interface("response", response).Str("event", event).Msg("")
-		if event ==  evt{
+		// log.Debug().Interface("response", response).Str("event", event).Str("evt", evt).Msg("")
+		if event == evt {
 			return response, nil
 		}
 	}
 }
-
 
 // func waitOriginateResponse(ctx context.Context, socket ami.Client, uuid string) (ami.Response, error) {
 // 	log.Debug().Str("uuid", uuid).Msg("Esperando Atención de llamada")
@@ -169,7 +181,6 @@ func waitForEvent(ctx context.Context, socket ami.Client, uuid string, event str
 // 		}
 // 	}
 // }
-
 
 // func waitForHangUp(ctx context.Context, socket ami.Client, uuid string) {
 // 	log.Debug().Str("uuid", uuid).Msg("Esperando finalizacion de llamada")
